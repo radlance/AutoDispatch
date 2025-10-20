1aa45256d4eb4ce03e79e620e9ad551313fa100a
Dmitry <manyakindima@gmail.com>
2025-10-20 22:19:11 +0300

    api: replace path with claim

--- AutoDispatchApi/src/main/kotlin/com/github/radlance/autodispatch/route/ProfileRoute.kt
+++ AutoDispatchApi/src/main/kotlin/com/github/radlance/autodispatch/route/ProfileRoute.kt
@@ -1,6 +1,7 @@
 package com.github.radlance.autodispatch.route
 
 import com.github.radlance.autodispatch.service.ProfileService
+import com.github.radlance.autodispatch.util.claimByNameOrUnauthorized
 import io.ktor.http.*
 import io.ktor.server.auth.*
 import io.ktor.server.response.*
@@ -9,14 +10,10 @@ import io.ktor.server.routing.*
 fun Route.profile(profileService: ProfileService) {
     authenticate {
         route("/users") {
-            get("/{login}") {
-                val login = call.parameters["login"]
-                    ?: run {
-                        call.respond(HttpStatusCode.BadRequest, "Login is missing")
-                        return@get
-                    }
+            get("/") {
+                val userLogin = call.claimByNameOrUnauthorized<String>(name = "login")
 
-                val user = profileService.userByLogin(login)
+                val user = profileService.userByLogin(userLogin)
                 call.respond(HttpStatusCode.OK, user)
             }
         }
--- AutoDispatchApi/src/main/kotlin/com/github/radlance/autodispatch/util/GetOrThrow.kt
+++ AutoDispatchApi/src/main/kotlin/com/github/radlance/autodispatch/util/GetOrThrow.kt
@@ -1,9 +1,24 @@
 package com.github.radlance.autodispatch.util
 
 import com.github.radlance.autodispatch.exception.MissingCredentialException
+import com.github.radlance.autodispatch.exception.UnauthorizedException
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
 import io.ktor.server.request.*
 
 suspend inline fun <reified T : Any> ApplicationCall.receiveOrThrow(): T {
     return runCatching { receive<T>() }.getOrElse { throw MissingCredentialException() }
+}
+
+inline fun <reified T : Any> ApplicationCall.claimByNameOrElse(name: String, action: () -> Nothing): T {
+    val principal = principal<JWTPrincipal>()
+    return principal?.getClaim(name, T::class) ?: action()
+}
+
+
+inline fun <reified T : Any> ApplicationCall.claimByNameOrUnauthorized(name: String): T {
+    return claimByNameOrElse<T>(name = name) {
+        throw UnauthorizedException()
+    }
 }
