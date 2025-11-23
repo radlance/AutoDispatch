52cbee8b591ae11b0f860bfa80ac0a2af1a6430b
Dmitry <manyakindima@gmail.com>
2025-11-23 12:53:20 +0300

    app: update sign in paddings

--- AutoDispatchApp/composeApp/src/mobileMain/kotlin/com/github/radlance/autodispatch/auth/presentation/SignInFields.kt
+++ AutoDispatchApp/composeApp/src/mobileMain/kotlin/com/github/radlance/autodispatch/auth/presentation/SignInFields.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
@@ -43,7 +42,7 @@ fun SignInFields(
     val keyboardController = LocalSoftwareKeyboardController.current
     var showPassword by rememberSaveable { mutableStateOf(false) }
 
-    Column(modifier = modifier.padding(18.dp)) {
+    Column(modifier = modifier) {
         OutlinedTextField(
             value = fieldsUiState.loginFieldValue,
             onValueChange = { onEvent(SignInEvent.ChangeLogin(it)) },
