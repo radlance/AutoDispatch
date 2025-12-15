6f57edabe93b0212fad62279cac8cb0d86e55cef
Dmitry <manyakindima@gmail.com>
2025-12-15 10:18:52 +0300

    app: add payloadCapacity to VehicleAssignmentFields

--- AutoDispatchApp/composeApp/src/jvmMain/kotlin/com/github/radlance/autodispatch/driver/assignment/presentation/VehicleAssignmentFields.kt
+++ AutoDispatchApp/composeApp/src/jvmMain/kotlin/com/github/radlance/autodispatch/driver/assignment/presentation/VehicleAssignmentFields.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
@@ -13,9 +15,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import autodispatch.composeapp.generated.resources.Res
 import autodispatch.composeapp.generated.resources.driver
 import com.github.radlance.autodispatch.request.change.presentation.CustomDropDownMenu
@@ -55,22 +59,37 @@ fun VehicleAssignmentFields(
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.spacedBy(12.dp)
             ) {
-                Text(
-                    text = currentOption.model,
-                    color = MaterialTheme.colorScheme.onSurface
-                )
-                Box(
-                    modifier = Modifier
-                        .clip(RoundedCornerShape(8.dp))
-                        .background(MaterialTheme.colorScheme.primaryContainer)
-                ) {
+                Column(modifier = Modifier.weight(3f)) {
                     Text(
-                        text = currentOption.licensePlate,
-                        maxLines = 1,
-                        color = MaterialTheme.colorScheme.onPrimaryContainer,
-                        overflow = TextOverflow.Ellipsis,
-                        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+                        text = currentOption.model,
+                        color = MaterialTheme.colorScheme.onSurface
                     )
+                    Spacer(Modifier.height(8.dp))
+                    Text(
+                        text = "г/п: ${currentOption.payloadCapacity} кг",
+                        color = MaterialTheme.colorScheme.onSurface,
+                        fontSize = 12.sp,
+                        modifier = Modifier.alpha(0.7f)
+                    )
+                }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.End,
+                    modifier = Modifier.weight(2f)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(8.dp))
+                            .background(MaterialTheme.colorScheme.primaryContainer)
+                    ) {
+                        Text(
+                            text = currentOption.licensePlate,
+                            maxLines = 1,
+                            color = MaterialTheme.colorScheme.onPrimaryContainer,
+                            overflow = TextOverflow.Ellipsis,
+                            modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+                        )
+                    }
                 }
             }
         }
