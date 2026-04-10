6a19ac319166250ea250f29e077ae858ce7877e6
Dmitry <manyakindima@gmail.com>
2026-04-10 12:15:55 +0300

    app: update dependencies and move Application.kt to project root

---
+++ AutoDispatchApi/src/main/kotlin/com/github/radlance/autodispatch/Application.kt
@@ -0,0 +1,27 @@
+package com.github.radlance.autodispatch
+
+import com.github.radlance.autodispatch.plugins.configureBroker
+import com.github.radlance.autodispatch.plugins.configureDatabases
+import com.github.radlance.autodispatch.plugins.configureDi
+import com.github.radlance.autodispatch.plugins.configureHTTP
+import com.github.radlance.autodispatch.plugins.configureRouting
+import com.github.radlance.autodispatch.plugins.configureSecurity
+import com.github.radlance.autodispatch.plugins.configureSerialization
+import com.github.radlance.autodispatch.plugins.configureValidation
+import io.ktor.server.application.*
+import io.ktor.server.netty.EngineMain
+
+fun main(args: Array<String>) {
+    EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureDatabases(config = environment.config)
+    configureDi()
+    configureBroker()
+    configureValidation()
+    configureSerialization()
+    configureHTTP()
+    configureSecurity()
+    configureRouting()
+}
